358f40fff22f2d5dca73371bc5af25008daaaebd
Giovana Franco Borges <giovana.borges@first-tech.net>
2022-09-14 16:48:12 -0300

    added loading component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Background } from './src/components/Background';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from "@expo-google-fonts/inter"
+
+import {Home} from "./src/screens/Home"
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+  })
+  
   return (
     <Background>
       <StatusBar 
@@ -10,6 +27,7 @@ export default function App() {
       backgroundColor="transparent"
       translucent
       />
+      {fontsLoaded}
     </Background>
   );
 }
